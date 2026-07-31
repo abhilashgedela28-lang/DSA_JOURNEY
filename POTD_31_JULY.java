cee3887a710e4291a05d2df626616472e3d4a758
Abhilash Gedela <abhilashgedela28@gmail.com>
2026-07-31 21:38:03 +0530

    Implement second approach with priority queue
    Added a second approach to calculate minimum pushes using a priority queue.

--- POTD_31_JULY.java
+++ POTD_31_JULY.java
@@ -1,4 +1,5 @@
 //3016
+// 1st approach 
 
 class Solution {
     public int minimumPushes(String word) {
@@ -27,5 +28,47 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+// 2nd approach using priority queue
+
+class Solution {
+    public int minimumPushes(String word) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
+            return b-a;
+        });
+        int[] arr = new int[26];
+        int ans = 0;
+        for(int i = 0;i<word.length();i++){
+            int ind = word.charAt(i) -'a';
+            arr[ind]++;
+        }
+        for(int i = 0;i<26;i++){
+            if(arr[i] != 0){
+                pq.add(arr[i]);
+            }
+        }
+        int count = 1;
+        int val = 1;
+        while(!pq.isEmpty()){
+            int x = pq.poll();
+            if(count <=8){
+                val = 1;
+            }
+            else if(count>8 && count <= 16){
+                val = 2;
+            }
+            else if(count >16 && count <= 24){
+                val = 3;
+            }
+            else{
+                val = 4;
+            }
+            ans += x*val;
+            count++;
+        }
+        return ans;
+       
     }
 }

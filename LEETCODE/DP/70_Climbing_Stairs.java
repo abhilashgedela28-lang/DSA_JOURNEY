1d75f5931c060cda5a8dea07e40c4c2a2d029e59
abhilash <abhilashgedela28@gmail.com>
2026-08-27 21:04:06 +0530

    committed

---
+++ LEETCODE/DP/70_Climbing_Stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev2 = 1;
+        int prev = 1;
+        int curr = 1;
+        for(int i = 2;i<=n;i++){
+            curr = prev2+prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
+    }
+}

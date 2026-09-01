c30e6bc8a882ed20dfd0c6645dd1d6325d039c3d
abhilash <abhilashgedela28@gmail.com>
2026-09-01 19:51:32 +0530

    updated

---
+++ LEETCODE/DP/1137_Nth_Tribonacci_Number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev1 = 1;
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        for(int i = 3;i<=n;i++){
+            int curr = prev3+prev2+prev1;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
+
